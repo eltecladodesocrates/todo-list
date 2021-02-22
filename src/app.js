1f4dd04a20831b0861f37c461f8cbe64086b1fad
Athos Conde de la Fere <eltecladodesocrates@gmail.com>
2021-02-22 00:30:51 -0500

    Connect data base

--- src/app.js
+++ src/app.js
@@ -1,24 +1,58 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const getDate = require('../utils/date')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-const items = ['Buy Food', 'Cook Food', 'Eat Food'] 
-const workItems = []
-
 app.set('view engine', 'ejs')
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
 
-    const day = getDate()
-    res.render('list', {
-        listTitle: day,
-        items
-    })
+const itemsSchema = {
+    name: String
+}
+
+const Item = mongoose.model('Item', itemsSchema)
+
+const item1 = new Item({
+    name: 'Go to work'
+})
+const item2 = new Item({
+    name: 'Make lunch'
+})
+const item3 = new Item({
+    name: 'Eat lunch'
+})
+
+const defaultItems = [item1, item2, item3]
+
+app.get('/', async (req, res) => {
+
+    const items = await Item.find()
+    if (items.length === 0) {
+        Item.insertMany(defaultItems, (err) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('Items succesfully saved in the database');
+                
+            }
+        })
+        res.redirect('/')
+    } else {
+        res.render('list', {
+            listTitle: 'Today',
+            items
+        })
+    }
 
 })
 
@@ -31,16 +65,20 @@ app.get('/work', (req, res) => {
 
 app.post('/', (req, res) => {
 
-    const item = req.body.newItem
+    const item = new Item({
+        name: req.body.newItem
+    })
+    item.save()
+    res.redirect('/')
 
-    if (req.body.list === 'Work') {
-        workItems.push(item)
-        res.redirect('/work')
-    } else {
-        items.push(item)
-        res.redirect('/')
-    }
-    
+})
+
+app.post('/delete', async (req, res) => {
+    const item = await Item.findByIdAndDelete({
+        _id: req.body.checkbox
+    })
+    console.log(`The task ${item.name} has been removed`);
+    res.redirect('/')
 })
 
 app.listen(port, () => {
